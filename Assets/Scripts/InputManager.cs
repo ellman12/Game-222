71532e261d1ce2a4300da1b1bb6b8356a14331df
Elliott DuCharme <ellduc4@gmail.com>
2023-01-12 13:39:06 -0600

    Class 1-12-2023

--- Assets/Scripts/InputManager.cs
+++ Assets/Scripts/InputManager.cs
@@ -1,6 +1,28 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class InputManager : MonoBehaviour
 {
-	
+	private PlayerInput playerInput;
+
+	private void Start()
+	{
+		//Initialization
+		playerInput = new PlayerInput();
+		playerInput.Enable();
+
+		//Subscribing
+		playerInput.Player.ButtonTest.performed += Pressed;
+	}
+
+	private void OnDisable()
+	{
+		playerInput.Player.ButtonTest.performed -= Pressed;
+	}
+
+	private void Pressed(InputAction.CallbackContext monkey)
+	{
+		Debug.Log("Pressed!");
+		transform.position += new Vector3(0, 1, 0);
+	}
 }
