a6956be4ff786219e3d1ef4104625a83da2aafaa
Elliott DuCharme <ellduc4@gmail.com>
2023-02-02 12:11:15 -0600

    Speed 🅱oost stuff in-class

---
+++ Assets/Scripts/GameActions/SpeedBoost_GA.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+
+namespace GameActions
+{
+	public class SpeedBoost_GA : GameAction
+	{
+		[SerializeField] private float speedBoostDuration = 2;
+		
+		public static Action SpeedBoostOn = delegate {  };
+		public static Action SpeedBoostOff = delegate {  };
+
+		private bool active;
+
+		public override void Action()
+		{
+			if (active) return;
+			SpeedBoostOn();
+			StartCoroutine(nameof(SpeedBoostOffDelay));
+		}
+
+		private IEnumerator SpeedBoostOffDelay()
+		{
+			active = true;
+			yield return new WaitForSeconds(speedBoostDuration);
+			SpeedBoostOff();
+			active = false;
+		}
+	}
+}
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,30 +1,44 @@
-﻿using UnityEngine;
+﻿using GameActions;
+using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
 	private Vector3 direction;
+	private bool speedBoost;
 	[SerializeField] private float normalSpeed, fastSpeed, currentSpeed;
 	private PlayerInput playerInput;
 
 	private void Start()
 	{
+		SpeedBoost_GA.SpeedBoostOn += SpeedBoostOn;
+		SpeedBoost_GA.SpeedBoostOff += SpeedBoostOff;
 		playerInput = new PlayerInput();
 		playerInput.Enable();
 		currentSpeed = normalSpeed;
-		
+
 		playerInput.Player.Faster.performed += _ => currentSpeed = fastSpeed;
 		playerInput.Player.Faster.canceled += _ => currentSpeed = normalSpeed;
 	}
 
 	private void OnDisable()
 	{
+		SpeedBoost_GA.SpeedBoostOn -= SpeedBoostOn;
+		SpeedBoost_GA.SpeedBoostOff -= SpeedBoostOff;
 		playerInput.Disable();
 	}
+
+	private void SpeedBoostOn() => speedBoost = true;
+
+	private void SpeedBoostOff() => speedBoost = false;
 
 	private void Update()
 	{
 		direction.x = playerInput.Player.Movement.ReadValue<Vector2>().x;
 		direction.y = playerInput.Player.Movement.ReadValue<Vector2>().y;
-		transform.position += direction * (currentSpeed * Time.deltaTime);
+		
+		if (speedBoost)
+			transform.position += direction * (fastSpeed * Time.deltaTime);
+		else
+			transform.position += direction * (normalSpeed * Time.deltaTime);
 	}
 }
